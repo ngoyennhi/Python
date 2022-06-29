93107b940239cc9eb29644e2f513fcc759a7efd8
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-06-29 14:06:11 +0200

    CS50

---
+++ hello.py
@@ -0,0 +1,85 @@
+
+import cs50
+# bt1 String Hello world
+# answer = cs50.get_string("What's your name?")
+# print(f"hello {answer}")
+# print("hello" + answer)
+# --------------------------------------
+# bt2 If 
+# x = cs50.get_int("What's the 1st number?")
+# y = cs50.get_int("What's the 2nd number?")
+# if x<y:
+#     print("x is less than y")
+# elif x>y:
+#     print("y is less than x")
+# else:
+#     print("y is equal to x")
+# --------------------------------------
+# bt3 While 
+# i = 0
+# while i < 3:
+#   print("hello world")
+#   i += 1
+# --------------------------------------
+# bt4 FOR 
+# for i in [0,1,2]:
+#     print("hello, world")
+
+# for i in range(3):
+#     print("hello, world")
+
+# from cs50 import get_int
+# x= get_int("x: ")
+# y= get_int("y: ")
+# print(x+y)
+# --------------------------------------
+# bt5 condition
+# from cs50 import get_string
+# s = get_string("Do you agree? ")
+# if s == "Y" or s == "y":
+#     print("Agreed.")
+# elif s == "N" or s == "n":
+#     print("Not agree")
+
+# from cs50 import get_string
+# s = get_string("Do you agree? ")
+# if s.lower() in ["y","yes"]:
+#     print("Agreed.")
+# elif s == "N" or s == "n":
+#     print("Not agree")    
+# --------------------------------------
+# bt6 function
+# def main():
+#     meow(3)
+# def meow(n):
+#     for i in range(3):
+#      print("meow")
+# main()
+# --------------------------------------
+# bt7 function
+# from cs50 import get_int
+# def main():
+#     i = get_positive_int()
+#     print(i)
+
+# def get_positive_int():
+#     while True:
+#         n = cs50.get_int("Positve Integer: ")
+#         if n > 0:
+#             break
+#     return n
+# main()
+# --------------------------------------
+# bt7 function cube 3x3
+# for i in range(3):
+#     for j in range(3):
+#         print("#", end="")
+#     print()
+# --------------------------------------
+# bt8 int
+# i = 1
+# while True:
+#     print(i)
+#     i *= 2
+# --------------------------------------
+# bt9 int
